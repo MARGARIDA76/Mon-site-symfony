f4e391e085c335b5f41f1477e5dc7fe4d436e330
Margarida Mbala <margaridambala76@gmail.com>
2022-09-14 15:15:08 +0200

    page detail

--- src/Controller/MissionController.php
+++ src/Controller/MissionController.php
@@ -2,17 +2,48 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Mission;
+use App\Repository\MissionRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
+#[Route('/mission')]
 class MissionController extends AbstractController
 {
-    #[Route('/mission', name: 'app_mission')]
-    public function index(): Response
+
+    #[Route('/', name: 'mission.list')]
+    public function index(ManagerRegistry $doctrine): Response 
+    
     {
-        return $this->render('mission/index.html.twig', [
-            'controller_name' => 'MissionController',
+
+        $repository = $doctrine->getRepository(persistentObject: Mission::class);
+        $missions = $repository->findAll();
+        return $this->render('mission/index.html.twig', ['missions' => $missions, 'isPaginated => true']);
+    }
+
+
+    
+    #[Route('/{id<\d+>}', name: 'mission.detail')]
+    public function detail(Mission $mission = null): Response 
+    
+    {
+
+        if(!$mission) {
+            $this->addFlash(type: 'error', message: "La mission  n'existe pas");
+            return $this->redirectToRoute('mission');
+        }
+        return $this->render('mission/detail.html.twig', ['mission' => $mission]);
+    }
+
+    #[Route('/detail', name: 'app_mission_detail', methods: ['GET'])]
+    public function details(MissionRepository $missionRepository, $id): Response
+    {
+        return $this->render('mission/detail.html.twig', [
+            'missions' => $missionRepository->findby($id),
         ]);
     }
+   
 }
